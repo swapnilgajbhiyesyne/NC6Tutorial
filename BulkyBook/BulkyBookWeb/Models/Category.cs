045e08ff232105e0654cbf10f5cc2854e794315e
sgajbhiye <sgajbhiye@legalzoom.com>
2023-03-20 20:36:22 +0530

    Added DataAnnotation

--- BulkyBook/BulkyBookWeb/Models/Category.cs
+++ BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,10 @@ namespace BulkyBookWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
-        public int Name { get; set; }
+        [Required]
+        public string Name { get; set; }
         public int DisplayOrder { get; set; }
         public DateTime CraetedDateTime { get; set; }
 
