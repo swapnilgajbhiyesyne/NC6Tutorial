ad2f3b11716cc034157e636ca1f52df4f25b3b6c
sgajbhiye <sgajbhiye@legalzoom.com>
2023-03-20 20:34:11 +0530

    Added Project

---
+++ BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CraetedDateTime { get; set; }
+
+    }
+}
