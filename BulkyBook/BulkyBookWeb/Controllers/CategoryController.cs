b916c84d578e68794a796075d11d43a7c6893820
sgajbhiye <sgajbhiye@legalzoom.com>
2023-03-20 22:29:08 +0530

    Retrive all categories

--- BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var categoriesList = _context.Categories.ToList();
             return View();
         }
     }
