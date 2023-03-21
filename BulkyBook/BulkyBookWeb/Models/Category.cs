a20fe35e87beb417aad5018af8265a8a40c7274c
sgajbhiye <sgajbhiye@legalzoom.com>
2023-03-21 14:23:39 +0530

    Added DataAnnotation DipslayName and Range

--- BulkyBook/BulkyBookWeb/Models/Category.cs
+++ BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace BulkyBookWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Dsiaply Order")]
+        [Range(1,20,ErrorMessage ="Qty must be within 1 to 50")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; }
 
