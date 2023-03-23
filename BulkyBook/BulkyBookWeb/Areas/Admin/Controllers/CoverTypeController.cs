7bd160ab4e46906454ea3095054b1fdfa303f92a
sgajbhiye <sgajbhiye@legalzoom.com>
2023-03-23 15:45:51 +0530

    Added CoverType Controller,Views Functionality

---
+++ BulkyBook/BulkyBookWeb/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,86 @@
+﻿using BulkyBook.DataAccess.Repository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    public class CoverTypeController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> coverTypes = _unitOfWork.CoverType.GetAll();
+            return View(coverTypes);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CoverType coverType)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Add(coverType);
+                _unitOfWork.Save();
+                TempData["success"] = "Cover Type Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(coverType);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == null) return NotFound();
+            var covertType = _unitOfWork.CoverType.GetFirstOrDefault(c => c.Id == id);
+            if (covertType == null) return NotFound();
+            _unitOfWork.CoverType.Update(covertType);
+            return View(covertType); 
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(CoverType coverType)
+        {
+            if(ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Update(coverType);
+                _unitOfWork.Save();
+                TempData["success"] = "Cover Type Updated Successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View(coverType);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if (id == null) return NotFound();
+            var coverType = _unitOfWork.CoverType.GetFirstOrDefault(c => c.Id == id);
+            if (coverType == null) return NotFound();
+            return View(coverType);
+        }
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteCoverType(int id)
+        {
+            if (id == null) return NotFound();
+            var coverType = _unitOfWork.CoverType.GetFirstOrDefault(c => c.Id == id);
+            if (coverType == null) return NotFound();
+            _unitOfWork.CoverType.Remove(coverType);
+            _unitOfWork.Save();
+            TempData["success"] = "CoverType Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
+
+    
+    
+
