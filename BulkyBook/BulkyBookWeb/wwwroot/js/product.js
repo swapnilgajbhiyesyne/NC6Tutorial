c13ff3f008ac9477293b65aeb5f9da6d86922056
sgajbhiye <sgajbhiye@legalzoom.com>
2023-03-25 21:49:22 +0530

    Pass data to action metod for edit

--- BulkyBook/BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBook/BulkyBookWeb/wwwroot/js/product.js
@@ -9,6 +9,21 @@ $(document).ready(function () {
             { data: "price" },
             { data: "author" },
             { data: "category.name" },
+            //pass data to action method for edit
+            {
+                "data": "id",
+                "render":
+                function (data) {
+                        return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                     <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+                    },
+                "width": "25%"
+            }
+
+
+            
         ],
     });
 });
