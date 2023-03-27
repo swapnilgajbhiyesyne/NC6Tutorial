4786b134fbb632edb1276b8a1827f97afd9935c6
Swapnil Gajbhiye <Swapnil.Gajbhiye@blueconchtech.com>
2023-03-27 23:20:38 +0530

    Added .Net Core Identity Librabry nad Scaffold Item in Web Project

--- BulkyBook/BulkyBookWeb/Program.cs
+++ BulkyBook/BulkyBookWeb/Program.cs
@@ -1,6 +1,7 @@
 using BulkyBook.Data;
 using BulkyBook.DataAccess.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,9 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
 ));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 //Register Repository
 //Removed as UOW is implemented
@@ -29,6 +33,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 //app.UseEndpoints(endpoints =>
